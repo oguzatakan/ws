3241f9c6250d44b3ae98ce4b5db53c4ca8f5fcc2
atakanoguz <atakan17171717@hotmail.com>
2022-09-06 17:00:42 +0300

    user update added

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,22 +1,28 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import com.hoaxify.ws.user.vm.UserVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api/1.0")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping("/api/1.0/users")
+    @PostMapping("/users")
     public GenericResponse createUser(@Valid @RequestBody User user) {
 
         userService.save(user);
@@ -24,9 +30,23 @@ public class UserController {
         return new GenericResponse("User Created");
     }
 
-    @GetMapping("/api/1.0/users")
+    @GetMapping("/users")
     Page<UserVM> getUsers(Pageable page,@CurrentUser User user){
         return userService.getUsers(page,user).map(UserVM::new);
+    }
+
+    @GetMapping("/users/{username}")
+    UserVM getUser(@PathVariable String username) {
+        User user = userService.getByUsername(username);
+        return new UserVM(user);
+    }
+
+    @PutMapping("/users/{username}")
+    @PreAuthorize("#username == principal.username")
+    UserVM updateUser(@RequestBody UserUpdateVM updatedUser, @PathVariable String username) {
+
+        User user = userService.updateUser(username, updatedUser);
+        return new UserVM(user);
     }
 
 }
--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.NotFoundException;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,5 +29,19 @@ public class UserService {
             return userRepository.findByUsernameNot(user.getUsername(),page);
         }
         return userRepository.findAll(page);
+    }
+
+    public User getByUsername(String username) {
+        User inDB = userRepository.findByUsername(username);
+        if (inDB == null) {
+            throw new NotFoundException();
+        }
+        return inDB;
+    }
+
+    public User updateUser(String username, UserUpdateVM updatedUser) {
+        User inDB = getByUsername(username);
+        inDB.setDisplayName(updatedUser.getDisplayName());
+        return userRepository.save(inDB);
     }
 }
