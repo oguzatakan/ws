af2add6ac7201f5d9b0bc4549ca70ec3551560b7
atakanoguz <atakan17171717@hotmail.com>
2022-09-07 17:52:44 +0300

    project text added

--- src/main/java/com/hoaxify/ws/WsApplication.java
+++ src/main/java/com/hoaxify/ws/WsApplication.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws;
 
+import com.hoaxify.ws.hoax.Hoax;
+import com.hoaxify.ws.hoax.HoaxService;
 import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserRepository;
 import com.hoaxify.ws.user.UserService;
@@ -17,7 +19,7 @@ public class WsApplication {
     }
 
     @Bean
-    CommandLineRunner createInitialUsers(UserService userService) {
+    CommandLineRunner createInitialUsers(UserService userService, HoaxService hoaxService) {
         return (args) -> {
                 for(int i = 1; i<=25; i++){
                     User user = new User();
@@ -25,6 +27,11 @@ public class WsApplication {
                     user.setDisplayName("display"+i);
                     user.setPassword("P4ssword");
                     userService.save(user);
+                }
+                for (int i = 1; i<=50; i++) {
+                    Hoax hoax = new Hoax();
+                    hoax.setContent("hoax - " +i);
+                    hoaxService.save(hoax);
                 }
             };
     }
