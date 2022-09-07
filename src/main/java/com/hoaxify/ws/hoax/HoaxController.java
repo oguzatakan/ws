ee4f23d162031438983c1cca42e0ac49595d1a12
atakanoguz <atakan17171717@hotmail.com>
2022-09-07 17:53:02 +0300

    chat text class added

---
+++ src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -0,0 +1,30 @@
+package com.hoaxify.ws.hoax;
+
+import com.hoaxify.ws.shared.GenericResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class HoaxController {
+
+    @Autowired
+    HoaxService hoaxService;
+
+    @PostMapping("/hoaxes")
+    GenericResponse saveHoax(@Valid @RequestBody Hoax hoax) {
+        hoaxService.save(hoax);
+        return new GenericResponse("Hoax is saved");
+
+    }
+
+    @GetMapping("/hoaxes")
+    Page<Hoax> getHoaxes(Pageable page){
+        return hoaxService.getHoaxes(page);
+    }
+
+}
